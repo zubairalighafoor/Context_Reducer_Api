85c39e00dc40025b9da76ef6f33d6ff2b52ac94a
zubair <55566847+zubairhunjrah6004@users.noreply.github.com>
2020-08-24 10:09:29 +0500

    Update CounterContext.js

--- src/CounterContext.js
+++ src/CounterContext.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+import {createContext} from 'react';
 
-// eslint-disable-next-line no-undef
 const counterContext = createContext(5);
 
 export default counterContext;
